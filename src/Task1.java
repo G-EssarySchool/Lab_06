98a7d25de66da49b390707d147114ded1b0a2667
G-EssarySchool <essary.619432@swl.k12.oh.us>
2025-09-30 08:18:28 -0400

    Task 1 complete

--- src/Task1.java
+++ src/Task1.java
@@ -3,3 +3,25 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        double tempC = 0;
+        double tempF = 0;
+        boolean done = false;
+
+        do {
+            System.out.println("Enter a temperature that will be converted from Celsius to Fahrenheit:");
+
+            if(scan.hasNextDouble()){
+                tempC = scan.nextDouble();
+                done = true;
+            }
+            else{
+                System.out.println("You must enter a valid temperature, please try again");
+                scan.nextLine();
+            }
+        } while (!done);
+        
+        tempF = (tempC * 9/5) + 32;
+        System.out.printf("%-13s%7.5s", "Degrees C:", tempC);
+        System.out.printf("\n%-13s%7.5s", "Degrees F:", tempF);
+    }
+}
