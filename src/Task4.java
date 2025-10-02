e4e0b5a78b2b847cb507e343679a26ce4542d92f
G-Essary <essary.619432@swl.k12.oh.us>
2025-10-02 15:01:33 -0400

    Finished Task 4

--- src/Task4.java
+++ src/Task4.java
@@ -1,52 +1,37 @@
 import java.util.Scanner;
 
+import java.lang.Math;
+
 public class Task4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int userGuess;
-        int randomNum;
-        boolean mango67 = false;
+        int randomNum = (int) (Math.random() * 10) + 1;
+        int userGuess = 0;
+        boolean done1 = false;
 
+        System.out.println("Welcome! Let's play a game.\nTry to guess my number!\nEnter a number [1-10]:");
+        
         do {
-            System.out.println("Welcome! Let's play a game.\nTry to guess my number!\nEnter a number [1-10]:");
-            userGuess = scan.nextInt();
-
-            if (userGuess <= 1 && 10 <= userGuess) {
-                System.out.println("");
-
-            }
-            else{
-                System.out.println("Sorry, you're guess is invalid. Please try again");
+            if (scan.hasNextInt()) {
+                userGuess = scan.nextInt();
+                if (userGuess >= 1 && 10 >= userGuess) {
+                    done1 = true;
+                } else {
+                    System.out.println("Sorry, you're guess is invalid. Please try again.");
+                    scan.nextLine();
+                }
+            } else {
+                System.out.println("Sorry, you're guess is invalid. Please try again.");
                 scan.nextLine();
             }
+        } while (!done1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        } while (!mango67);
+        if (userGuess > randomNum) {
+            System.out.println("My number was " + randomNum + ". \nYour guess was too high.");
+        } else if (userGuess < randomNum) {
+            System.out.println("My number was " + randomNum + ". \nYour guess was too low.");
+        } else {
+            System.out.println("My number was " + randomNum + ". \nYou got it right!");
+        }
     }
 }
