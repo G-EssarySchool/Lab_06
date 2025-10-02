0b404f9d16ae5600c49b33e5c3db34cc1cde3c59
G-EssarySchool <essary.619432@swl.k12.oh.us>
2025-10-02 08:52:51 -0400

    Task 4 update

--- src/Task4.java
+++ src/Task4.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 public class Task4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int userGuess;
+        int randomNum;
+        boolean mango67 = false;
+
+        do {
+            System.out.println("Welcome! Let's play a game.\nTry to guess my number!\nEnter a number [1-10]:");
+            userGuess = scan.nextInt();
+
+            if (userGuess <= 1 && 10 <= userGuess) {
+                System.out.println("");
+
+            }
+            else{
+                System.out.println("Sorry, you're guess is invalid. Please try again");
+                scan.nextLine();
+            }
 
 
 
@@ -31,12 +47,6 @@ public class Task4 {
 
 
 
-
-
-
-
-
-
-
+        } while (!mango67);
     }
 }
